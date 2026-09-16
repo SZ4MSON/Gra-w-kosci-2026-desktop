e212ee6df9654cac0f1cc974f43ee9497fc98002
SZ4MSON <szymonpaluch09@icloud.com>
2026-09-16 17:06:55 +0200

    Integrate Kosc model with MainWindow UI handlers

--- Gra w kosci 2026 desktop/MainWindow.xaml.cs
+++ Gra w kosci 2026 desktop/MainWindow.xaml.cs
@@ -20,14 +20,47 @@ namespace Gra_w_kosci_2026_desktop
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Kosc[] dices = {
+            new Kosc(0),
+            new Kosc(0),
+            new Kosc(0),
+            new Kosc(0),
+            new Kosc(0)
+        };
+
+        public Image[] diceElements;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            diceElements = new Image[] {
+                kosc1, kosc2, kosc3, kosc4, kosc5
+            };
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void BtnRoll_Click(object sender, RoutedEventArgs e)
         {
+            int total = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Kosc dice = dices[i];
+                Image diceElement = diceElements[i];
 
+                dice.throwDice();
+                diceElement.Source = new BitmapImage(new Uri(dice.filenames[dice.currentDiceFileIdx], UriKind.Relative));
+                total += dice.currentDice;
+            }
+            txtResult.Text = total.ToString();
+        }
+
+        private void OnDiceClick(object sender, MouseButtonEventArgs e)
+        {
+            Image img = (Image)sender;
+            int tag = Convert.ToInt32(img.Tag);
+
+            dices[tag].toggleLock();
+            img.Opacity = dices[tag].diceAvailable ? 1.0 : 0.5;
         }
     }
 }
